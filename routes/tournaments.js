082a6672735abc50ab94226bc1be89308b6977a0
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-04-08 22:15:49 -0600

    seed data

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -23,7 +23,53 @@ const tournaments = [
             forRound: (-1),
             forTournament: (-1)
         }]
-    }
+    },
+    {
+        id: 2,
+        name: 'Cherry Hills Club Championship',
+        venue: 0,
+        players: [{
+            id: 0,
+            score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 6, 4, 3, 4]],
+            forRound: 4,
+            forTournament: 4
+        },
+        {
+            id: 2,
+            score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 4, 4, 3, 4]],
+            forRound: 2,
+            forTournament: 2
+        },
+        {
+            id: 5,
+            score: [[4, 4, 5, 3, 4, 6, 3, 4, 3, 4, 5, 2, 4, 5, 4, 4, 3, 4]],
+            forRound: (-1),
+            forTournament: (-1)
+        }]
+    },
+    {
+        id: 3,
+        name: 'Sykes Invitational',
+        venue: 0,
+        players: [{
+            id: 0,
+            score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 6, 4, 3, 4]],
+            forRound: 4,
+            forTournament: 4
+        },
+        {
+            id: 2,
+            score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 4, 4, 3, 4]],
+            forRound: 2,
+            forTournament: 2
+        },
+        {
+            id: 5,
+            score: [[4, 4, 5, 3, 4, 6, 3, 4, 3, 4, 5, 2, 4, 5, 4, 4, 3, 4]],
+            forRound: (-1),
+            forTournament: (-1)
+        }]
+    },
 ]
 
 router.get('/', (req, res) => {
