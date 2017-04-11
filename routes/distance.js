fe8369fb8e32b6c1db7215402625fb70e059ee59
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-04-10 23:35:12 -0600

    created Radian converter

--- routes/distance.js
+++ routes/distance.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 let getDistance = (lat1, lat2, lon1, lon2) => {
-    let R = 6371e3; // metres
-    let φ1 = lat1.toRadians();
-    let φ2 = lat2.toRadians();
-    let Δφ = (lat2 - lat1).toRadians();
-    let Δλ = (lon2 - lon1).toRadians();
+    let R = 6371e3,
+        φ1 = lat1 * Math.PI / 180,
+        φ2 = lat2 * Math.PI / 180,
+        Δφ = (lat2 - lat1) * Math.PI / 180,
+        Δλ = (lon2 - lon1) * Math.PI / 180
 
     let a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
         Math.cos(φ1) * Math.cos(φ2) *
