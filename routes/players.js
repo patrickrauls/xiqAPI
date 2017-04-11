679a7d4cdb3693f22de208bdc99bd2bb01cba7eb
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-04-11 16:43:32 -0600

    get one player

--- routes/players.js
+++ routes/players.js
@@ -4,94 +4,11 @@ const router = require('express').Router(),
     assert = require('assert'),
     uri = process.env.URI
 
-const players =
-    [
-        {
-            id: 0,
-            name: {
-                first: 'Patrick',
-                middle: 'Andrew',
-                last: 'Rauls',
-                nickname: 'PAR'
-            },
-            handicap: 4.3,
-            location: 'Denver, CO'
-        },
-        {
-            id: 1,
-            name: {
-                first: 'Richard',
-                middle: null,
-                last: 'Beese',
-                nickname: 'BZ'
-            },
-            handicap: 8.1,
-            location: 'Denver, CO'
-        },
-        {
-            id: 2,
-            name: {
-                first: 'Sarah',
-                middle: null,
-                last: 'Healey',
-                nickname: ''
-            },
-            handicap: 8.1,
-            location: 'Denver, CO'
-        },
-        {
-            id: 3,
-            name: {
-                first: 'Jeff',
-                middle: null,
-                last: '"Mettle" Diers',
-                nickname: ''
-            },
-            handicap: 8.1,
-            location: 'Denver, CO'
-        },
-        {
-            id: 4,
-            name: {
-                first: 'Kim',
-                middle: null,
-                last: 'Schlesinger',
-                nickname: ''
-            },
-            handicap: 8.1,
-            location: 'Denver, CO'
-        },
-        {
-            id: 5,
-            name: {
-                first: 'Carlos',
-                middle: null,
-                last: 'Rodrigues',
-                nickname: ''
-            },
-            handicap: 8.1,
-            location: 'Denver, CO'
-        },
-        {
-            id: 6,
-            name: {
-                first: 'Berto',
-                middle: null,
-                last: 'Ortega',
-                nickname: ''
-            },
-            handicap: 8.1,
-            location: 'Denver, CO'
-        }
-    ]
 
 router.get('/', (req, res) => {
     let ids = req.query.ids.map(id => Number(id)),
         invitees = [];
-    console.log(ids)
-    // req.query.ids.forEach(id => {
-    //     invitees.push(players[id])
-    // })
+
     mongo.connect(uri, (err, db) => {
         assert.equal(null, err);
         let cursor = db.collection('players').find({
@@ -108,6 +25,13 @@ router.get('/', (req, res) => {
     })
 })
 router.patch('/', (req, res) => {
-
+    mongo.connect(uri, (err, db) => {
+        assert.equal(null, err);
+        db.collection('players').findOne(req.body)
+            .then(player => {
+                console.log(player);
+                res.status(200).json(player);
+            })
+    })
 })
 module.exports = router;
