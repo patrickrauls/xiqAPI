5351e1279c35f2808b7fa849202b7bfb331679ff
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-04-12 12:27:11 -0600

    Added get specific tournament route

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -4,118 +4,6 @@ const router = require('express').Router(),
     assert = require('assert'),
     uri = process.env.URI;
 
-const tournaments =
-    [
-        {
-            id: 1,
-            name: 'Denver Invitational',
-            venue: 0,
-            start: 'May 7',
-            end: 'May 10, 2017',
-            rules: ['Stroke Play', 'Gold Tees', 'Lift, Clean, and Place', 'Cart Path Only', 'All Waste Bunkers are Out of Bounds', 'Putt It Out'],
-            players: [{
-                id: 0,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 6, 4, 3, 4]],
-                forRound: 4,
-                forTournament: 4
-            },
-            {
-                id: 2,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 4, 4, 3, 4]],
-                forRound: 2,
-                forTournament: 2
-            },
-            {
-                id: 5,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 3, 4, 5, 2, 4, 5, 4, 4, 3, 4]],
-                forRound: (-1),
-                forTournament: (-1)
-            }]
-        },
-        {
-            id: 2,
-            name: 'Cherry Hills Club Championship',
-            venue: 0,
-            start: 'April 20',
-            end: 'April 23, 2017',
-            rules: ['Stroke Play', 'Gold Tees', 'Lift, Clean, and Place', 'Cart Path Only', 'All Waste Bunkers are Out of Bounds', 'Putt It Out'],
-            players: [{
-                id: 0,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 6, 4, 3, 4]],
-                forRound: 4,
-                forTournament: 4
-            },
-            {
-                id: 2,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 4, 4, 3, 4]],
-                forRound: 2,
-                forTournament: 2
-            },
-            {
-                id: 5,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 3, 4, 5, 2, 4, 5, 4, 4, 3, 4]],
-                forRound: (-1),
-                forTournament: (-1)
-            }]
-        },
-        {
-            id: 3,
-            name: 'Sykes Invitational',
-            venue: 0,
-            start: 'April 13',
-            end: 'April 16, 2017',
-            rules: ['Stroke Play', 'Gold Tees', 'Lift, Clean, and Place', 'Cart Path Only', 'All Waste Bunkers are Out of Bounds', 'Putt It Out'],
-            players: [{
-                id: 0,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 6, 4, 3, 4]],
-                forRound: 4,
-                forTournament: 4
-            },
-            {
-                id: 2,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 5, 5, 5, 2, 4, 5, 4, 4, 3, 4]],
-                forRound: 2,
-                forTournament: 2
-            },
-            {
-                id: 5,
-                score: [[4, 4, 5, 3, 4, 6, 3, 4, 3, 4, 5, 2, 4, 5, 4, 4, 3, 4]],
-                forRound: (-1),
-                forTournament: (-1)
-            }],
-            teeTimes: [
-                [
-                    {
-                        players: [0, 1, 2],
-                        hole: 2,
-                        time: '07:39:00'
-
-                    },
-                    {
-                        players: [3, 4, 5],
-                        hole: 3,
-                        time: '07:39:00'
-
-                    }
-                ],
-                [
-                    {
-                        players: [0, 1, 2],
-                        hole: 7,
-                        time: '07:39:00'
-
-                    },
-                    {
-                        players: [3, 4, 5],
-                        hole: 8,
-                        time: '07:39:00'
-
-                    }
-                ]
-
-            ]
-        }
-    ]
 
 router.get('/', (req, res) => {
     let tournaments = [];
@@ -132,16 +20,22 @@ router.get('/', (req, res) => {
     })
 
 })
-// router.post('/score', (req, res) => {
-//     let score = req.body.score
-//     mongo.connect(uri, (err, db) => {
-//         assert.equal(null, err);
-//         db.collection('tournaments').updateOne({/* identifying info*/}, {$set: score} (err, result) => {
-//             assert.equal(null, err);
-//             console.log('item inserted');
-//             db.close();
-//         })
-//     })
-//     res.status(200).json(tournaments);
-// })
+router.get('/:id', (req, res) => {
+    mongo.connect(uri, (err, db) => {
+        assert.equal(null, err);
+        db.collection('tournaments').findOne({
+            '_id': objectId(req.params.id)
+        })
+            .then(tournament => {
+                db.close();
+                res.status(200).json(tournament)
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).json(err)
+            })
+    })
+
+})
+
 module.exports = router;
